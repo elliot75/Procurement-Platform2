d7cac94ef2eccb0d045580fd233f078773527587
Liang, Yi Chang <elliot64@gmail.com>
2026-09-02 12:28:44 +0700

    security: make DB TLS verification configurable (secure by default)

--- server/db.js
+++ server/db.js
@@ -5,11 +5,19 @@ dotenv.config();
 
 const { Pool } = pg;
 
+function buildSslConfig() {
+    if (process.env.DATABASE_SSL === 'disable') {
+        return false;
+    }
+    // Default to verifying certificates. Set DB_SSL_REJECT_UNAUTHORIZED=false only if required.
+    return {
+        rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED !== 'false'
+    };
+}
+
 export const pool = new Pool({
     connectionString: process.env.DATABASE_CONNECTION_STRING,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    ssl: buildSslConfig()
 });
 
 // Helper for single query
